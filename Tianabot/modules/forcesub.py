23f1274c37254990d5e8807b1f65d5396b7a1574
Mhd Althaf <93602145+Muhammedalthaf174@users.noreply.github.com>
2022-05-22 17:12:19 +0530

    Update forcesub.py

--- Tianabot/modules/forcesub.py
+++ Tianabot/modules/forcesub.py
@@ -197,5 +197,15 @@ __help__ = """
  ❍ /fsub disable - To turn of ForceSubscribe..
   💡If you disable fsub, you need to set again for working.. /fsub {channel username} 
  ❍ /fsub clear - To unmute all members who muted by me.
+Everything is fun, until a spammer starts entering your group, and you have to block it. Then you need to start banning more, and more, and it hurts.
+But then you have many groups, and you don't want this spammer to be in one of your groups - how can you deal? Do you have to manually block it, in all your groups?\n
+*No longer!* With Federation, you can make a ban in one chat overlap with all other chats.\n
+You can even designate federation admins, so your trusted admin can ban all the spammers from chats you want to protect.\n
+*Commands:*\n
+Feds are now divided into 3 sections for your ease.
+• `/fedownerhelp`*:* Provides help for fed creation and owner only commands
+• `/fedadminhelp`*:* Provides help for fed administration commands
+• `/feduserhelp`*:* Provides help for commands anyone can use
 """
-__mod_name__ = "F Sᴜʙ"
+__mod_name__ = "ғ-sᴜʙ/ғᴇᴅs"
+
