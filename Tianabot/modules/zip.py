524c61031871a6e0453c4e8cf5df2af7bb27cd93
Mhd Althaf <93602145+Muhammedalthaf174@users.noreply.github.com>
2022-05-22 20:46:41 +0530

    Update zip.py

--- Tianabot/modules/zip.py
+++ Tianabot/modules/zip.py
@@ -43,7 +43,7 @@ async def _(event):
     if event.is_group:
         if not (await is_register_admin(event.input_chat, event.message.sender_id)):
             await event.reply(
-                "Hey, you are not admin. You can't use this command, But you can use in my PM 🙂"
+                "Hey, You are not admin. You can't use this command, But you can use in my pm 🙂"
             )
             return
 
@@ -125,7 +125,7 @@ async def _(event):
     if event.is_group:
         if not (await is_register_admin(event.input_chat, event.message.sender_id)):
             await event.reply(
-                "Hey, You are not admin. You can't use this command, But you can use in my PM 🙂"
+                "Hey, You are not admin. You can't use this command, But you can use in my pm 🙂"
             )
             return
 
@@ -207,12 +207,3 @@ def get_lst_of_files(input_directory, output_lst):
             return get_lst_of_files(current_file_name, output_lst)
         output_lst.append(current_file_name)
     return output_lst
-
-
-__help__ = """
-Hey I can convert files here..
- ❍ /zip*:* reply to a telegram file to compress it in .zip format
- ❍ /unzip*:* reply to a telegram file to decompress it from the .zip format
-"""
-
-__mod_name__ = "Zɪᴘᴘᴇʀ​"
